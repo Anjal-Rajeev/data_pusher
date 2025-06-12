bac5af5b7cd88f92be727e6b0cc409759d87cbac
Anjal-srv <anjal@srvinfotech.in>
2025-06-13 01:52:56 +0530

    login api

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import { asyncErrorHandler, Error, Response } from "express-error-catcher";
 import models from "../models/index.js";
 import { SignupBodyValidation, LoginBodyValidation } from "../utils/validationSchema.js";
+import jwt from "jsonwebtoken"
+import { ACCESS_TOKEN_SECRET } from "../config.js";
 
 
 export const signUp = asyncErrorHandler(async (req) => {
@@ -16,29 +18,32 @@ export const signUp = asyncErrorHandler(async (req) => {
 })
 
 
-export const updateAccount = asyncErrorHandler(async (req) => {
-  const { id, email, name, website } = req.body;
-  const { error } = AccountBodyValidation(req.body);
-  if (error) throw new Error(error.details.map(err => err.message.replace(/['"]/g, "")), 400);
+export const login = asyncErrorHandler(async (req) => {
+  const { email, password } = req.body;
+  const user = await models.Users.findOne({ email, status: 0 });
+  if (!user) throw new Error("Account not found", 400);
+  if (!(await user.comparePassword(password))) throw new Error("Invalid Credentials", 400);
 
-  // Checking if the account exists or not
-  const account = await Account.findOne({
-    where: { id, status: 0 },
+  const token = jwt.sign({ id: user._id, email: user.email }, ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
   });
-  if (!account) throw new Error("Account not found", 404);
 
-  // Checking for duplicate accounts
-  const existingAccount = await Account.findOne({
-    where: { email, status: 0, id: { [Op.ne]: id } },
-  });
-  if (existingAccount) throw new Error("An account with this email already exists", 409);
-
-  account.email = email;
-  account.name = name;
-  account.website = website;
-
-  await account.save();
-
-  return new Response("Account updated successfully", null, 200);
+  return new Response("Login successful", null, 200);
 })
 
+
+export const logout = asyncErrorHandler(async (req, res) => {
+  // Clear the cookie
+  res.clearCookie('token', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+  });
+
+  return new Response("Logged out successfully", null, 200);
+});
