9bbc5e6b587844e536ad7b74141ba4e13c7621c1
Anjal-srv <anjal@srvinfotech.in>
2025-06-13 01:55:53 +0530

    user models update

--- models/User.js
+++ models/User.js
@@ -1,10 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import bcrypt from "bcrypt"
+import { ROLES } from '../config.js';
 
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role: { type: Schema.Types.ObjectId, ref: "roles", default: ROLES.ADMIN },
 
   created_by: { type: String },
   updated_by: { type: String },
